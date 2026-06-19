3aa4366e30b37439510a49c708cd8f2b138157f3
canbekcan <can@canbekcan.com>
2026-06-19 10:13:10 +0300

    fix

--- assets/javascripts/discourse/api-initializers/orcid-publications-init.js
+++
@@ -1,7 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-
-export default apiInitializer("1.15.0", (api) => {
-  api.addRoutes("user", function () {
-    this.route("orcidPublications", { path: "/publications" });
-  });
-});
