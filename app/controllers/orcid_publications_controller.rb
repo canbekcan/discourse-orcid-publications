e2537dc99a66c2d186f38f0ea76be69075b1e10d
canbekcan <can@canbekcan.com>
2026-07-07 23:58:05 +0300

    v0.1.3

--- app/controllers/orcid_publications_controller.rb
+++ app/controllers/orcid_publications_controller.rb
@@ -11,7 +11,8 @@ class OrcidPublicationsController < ::ApplicationController
     # custom_field_id: 3 veritabanında 'user_field_3' olarak tutulur.
     # raw_orcid = user.custom_fields["user_field_3"]
     field_name = SiteSetting.orcid_custom_field_name
-    raw_orcid = user.custom_fields[field_name]
+    user.custom_fields[field_name] = extracted_orcid_id
+    user.save_custom_fields
     orcid_id = nil
 
     if raw_orcid.present?
