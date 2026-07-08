36779adb697c61289ef97a106748c1a89f3db9fd
canbekcan <can@canbekcan.com>
2026-07-09 00:14:28 +0300

    orcid connect bind

--- app/controllers/orcid_publications_controller.rb
+++ app/controllers/orcid_publications_controller.rb
@@ -11,7 +11,8 @@ class OrcidPublicationsController < ::ApplicationController
     # custom_field_id: 3 veritabanında 'user_field_3' olarak tutulur.
     # raw_orcid = user.custom_fields["user_field_3"]
     # Bu kısımda düzenleme yapılmalı. User field kısmını orcid-connect eklentisinden belirtilen alanla eşleştirilmeli.
-    field_name = SiteSetting.orcid_custom_field_name
+    # field_name = SiteSetting.orcid_custom_field_name
+    field_name = SiteSetting.orcid_connect_user_field_mappings.presence
     raw_orcid = user.custom_fields[field_name]
     orcid_id = nil
 
